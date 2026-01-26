484e51138f3eb59c97236ed631e1b8d6267159e4
Evann Abrial <evann.abrial@cybevasion.fr>
2026-01-26 13:59:49 +0100

    Ajout projet Console + Lib avec exemple ✨

---
+++ ClassLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary;
+
+public class Person
+{
+    private string _nom;
+
+    public string Nom
+    {
+        get => _nom;
+        set => _nom = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassLibrary;
+
+Person p = new Person();
+p.Nom = "Elio";
+
+Console.WriteLine(p.Nom);
