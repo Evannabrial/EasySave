6ab31222b4c80b43980700c525a5026c5337b5bb
Evann Abrial <evann.abrial@cybevasion.fr>
2026-02-10 11:27:44 +0100

    Ajout du projet EasySave (Avalonia MVVM) :airplane: :sparkles:

---
+++ EasySave/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EasySaveLibrary;
+using EasySave.Services;
+using EasySaveLibrary.Model;
+
+namespace EasySave.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private readonly JobManager _jobManager;
+
+    // On expose la liste pour que la Vue puisse la voir
+    public ObservableCollection<Job> Jobs { get; set; }
+
+    public MainWindowViewModel(JobManager jobManager)
+    {
+        _jobManager = jobManager;
+        
+        // On synchronise ou on référence la liste du modèle
+        Jobs = new ObservableCollection<Job>(_jobManager.LJobs);
+    }
+}
