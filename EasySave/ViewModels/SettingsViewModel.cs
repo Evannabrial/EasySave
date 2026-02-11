2b4e8f28edb27f50ef500742aed4b26bb61ddef1
FrozX69 <elio.fvr@gmail.com>
2026-02-11 17:20:24 +0100

    debut trad dsl evann g oublié de creer une autre branche je le fais au prochain commit promis

--- EasySave/ViewModels/SettingsViewModel.cs
+++ EasySave/ViewModels/SettingsViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using EasySaveLibrary;
 
 namespace EasySave.ViewModels;
@@ -5,9 +7,20 @@ namespace EasySave.ViewModels;
 public class SettingsViewModel : ViewModelBase
 {
     private readonly JobManager _jobManager;
+    private Dictionary<string, string> _dictText;
+
+    public JobManager JobManager => _jobManager;
+
+    public Dictionary<string, string> DictText
+    {
+        get => _dictText;
+        set => _dictText = value ;
+    }
 
     public SettingsViewModel(JobManager jobManager)
     {
         _jobManager = jobManager;
+        DictText = jobManager.Language.GetTranslations();
+
     }
 }
