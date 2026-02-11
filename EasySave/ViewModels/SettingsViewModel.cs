022306100438538749569866eb58a5425240d7b7
FrozX69 <elio.fvr@gmail.com>
2026-02-11 14:29:19 +0100

    Vue Settings

--- EasySave/ViewModels/SettingsViewModel.cs
+++ EasySave/ViewModels/SettingsViewModel.cs
@@ -5,7 +5,7 @@ namespace EasySave.ViewModels;
 public class SettingsViewModel : ViewModelBase
 {
     private readonly JobManager _jobManager;
-    
+
     public SettingsViewModel(JobManager jobManager)
     {
         _jobManager = jobManager;
